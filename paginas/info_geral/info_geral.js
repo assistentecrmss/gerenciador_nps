eeeeef3bc0e8b61e6b33a99cb3715a24800f3144
Arthur Venâncio <arthurmvenancio@gmail.com>
2025-06-26 11:50:30 -0300

    refactor: retirou a importação de links gerais da info_geral

--- paginas/info_geral/info_geral.js
+++ paginas/info_geral/info_geral.js
@@ -1,62 +1,82 @@
-import links from "../../db/links.json" with {type: 'json'}
 import lojas from "../../db/lojas.json" with {type: 'json'}
 
-function preencherInfoGeral() {
+
+async function pegarLink(loja) {
+    const response = await fetch("/api/links", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ loja })
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.error || "Erro ao buscar links");
+    }
+
+    return data;
+}
+
+
+async function preencherInfoGeral() {
     const chave_loja = sessionStorage.getItem("loja_selecionada")
+    const links = await pegarLink(chave_loja)
+    const link_rede = await pegarLink("l_0")
 
     document.getElementById("titulo").textContent = lojas[chave_loja]
+    
+    criarRespostas(links)
+    criarNpsAtual(links)
+    criarNpsTotal(links)
 
-    criarRespostas(chave_loja)
-    criarNpsAtual(chave_loja)
-    criarNpsTotal(chave_loja)
-
-    criarPreferencias(chave_loja)
+    criarPreferencias(links)
 
     criarBotaoHistoricoSetor()
-    criarSetor(chave_loja, "espera")
-    criarSetor(chave_loja, "hortifruti")
-    criarSetor(chave_loja, "climatizacao")
-    criarSetor(chave_loja, "limpeza")
-    criarSetor(chave_loja, "atendimento")
-    criarSetor(chave_loja, "acogue")
+    criarSetor(chave_loja, "espera", links, link_rede)
+    criarSetor(chave_loja, "hortifruti", links, link_rede)
+    criarSetor(chave_loja, "climatizacao",  links, link_rede)
+    criarSetor(chave_loja, "limpeza", links, link_rede)
+    criarSetor(chave_loja, "atendimento", links, link_rede)
+    criarSetor(chave_loja, "acogue", links, link_rede)
 
     if (chave_loja == "l_0") { document.getElementById("ranking_nps").style.display = "flex" }
 
 }
 
-function criarRespostas(chave_loja) {
+function criarRespostas(links) {
     const container = document.getElementById("respostas")
 
     const resp_atual = document.createElement("img")
-    resp_atual.src = links[chave_loja].respostas.atual
+    resp_atual.src = links.respostas.atual
     resp_atual.classList.add("respostas")
     container.appendChild(resp_atual)
 
     const resp_anterior = document.createElement("img")
-    resp_anterior.src = links[chave_loja].respostas.anterior
+    resp_anterior.src = links.respostas.anterior
     resp_anterior.classList.add("respostas")
     container.appendChild(resp_anterior)
 
     const resp_total = document.createElement("img")
-    resp_total.src = links[chave_loja].respostas.total
+    resp_total.src = links.respostas.total
     resp_total.classList.add("respostas")
     container.appendChild(resp_total)
 }
-function criarNpsAtual(chave_loja) {
+function criarNpsAtual(links) {
     const nps_atual = document.createElement("img")
-    nps_atual.src = links[chave_loja].nps.atual
+    nps_atual.src = links.nps.atual
     nps_atual.classList.add("indicador_setor")
     document.getElementById("nps_atual").appendChild(nps_atual)
 
     const variacao = document.createElement("img")
-    variacao.src = links[chave_loja].nps.variacao
+    variacao.src = links.nps.variacao
     variacao.classList.add("indicador_setor")
     variacao.style.width = "100%"
     document.getElementById("nps_atual").appendChild(variacao)
 }
-function criarNpsTotal(chave_loja) {
+function criarNpsTotal(links) {
     const nps_total = document.createElement("img")
-    nps_total.src = links[chave_loja].nps.total
+    nps_total.src = links.nps.total
     nps_total.classList.add("indicador_setor")
     document.getElementById("nps_total").appendChild(nps_total)
 
@@ -69,35 +89,35 @@ function criarNpsTotal(chave_loja) {
     })
     document.getElementById("nps_total").appendChild(historico_nps)
 }
-function criarPreferencias(chave_loja) {
+function criarPreferencias(links) {
     const preferencia = document.createElement("img")
-    preferencia.src = links[chave_loja].preferencias
+    preferencia.src = links.preferencias
     preferencia.classList.add("indicador_setor")
     document.getElementById("preferencias").appendChild(preferencia)
 
 }
-function criarSetor(chave_loja, setor) {
+function criarSetor(chave_loja, setor, links,link_rede) {
     const container = document.getElementById(String(setor))
     if (chave_loja == "l_0") {
         const indicador = document.createElement("img")
-        indicador.src = links[chave_loja][setor].indicador
+        indicador.src = links[setor].indicador
         indicador.style.width = "80%"
         container.appendChild(indicador)
         return
     }
 
     const ranking = document.createElement("img")
-    ranking.src = links[chave_loja][setor].ranking
+    ranking.src = links[setor].ranking
     ranking.style.width = "80%"
     container.appendChild(ranking)
 
     const indicador = document.createElement("img")
-    indicador.src = links[chave_loja][setor].indicador
+    indicador.src = links[setor].indicador
     indicador.style.width = "80%"
     container.appendChild(indicador)
 
     const bench = document.createElement("img")
-    bench.src = links["l_0"][setor].bench
+    bench.src = link_rede[setor].bench
     bench.style.width = "80%"
     container.appendChild(bench)
 }
