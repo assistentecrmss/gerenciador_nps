d9c89809f9f9f2157653f3374a6dda633f855bef
Arthur Venâncio <arthurmvenancio@gmail.com>
2025-06-26 16:02:25 -0300

    refactor: ajustando histórico de nps para rota backend

--- paginas/historico_nps/historico_nps.js
+++ paginas/historico_nps/historico_nps.js
@@ -1,59 +1,60 @@
-import links from "../../db/links.json" with {type: 'json'}
+import { pegarLink } from "../../lib/pegarLinks.js"
 import lojas from "../../db/lojas.json" with {type: 'json'}
 
-function preencherHistoricoNPS(){
+async function preencherHistoricoNPS(){
     const chave_loja = sessionStorage.getItem("loja_selecionada")
+    const links = await pegarLink(chave_loja)
 
     document.getElementById("titulo").textContent = lojas[chave_loja]
 
-    criarHistorico(chave_loja)
-    criarDivisaoPublico(chave_loja)
-    historicoPublico(chave_loja)
+    criarHistorico(links)
+    criarDivisaoPublico(links)
+    historicoPublico(links)
 
 }
 
-function criarHistorico(chave_loja){
+function criarHistorico(links){
     const container = document.createElement("div")
     container.classList.add("horizontal")
     container.style.padding="1rem"
     container.style.marginInline="1rem"
 
     const historico = document.createElement("img")
-    historico.src=links[chave_loja].nps.historico
+    historico.src=links.nps.historico
     container.appendChild(historico)
 
     const resp_total = document.createElement("img")
-    resp_total.src = links[chave_loja].respostas.total
+    resp_total.src = links.respostas.total
     container.appendChild(resp_total)
 
     document.getElementById("container").appendChild(container)
 
 }
 
-function criarDivisaoPublico(chave_loja){
+function criarDivisaoPublico(links){
     const container = document.createElement("div")
     container.classList.add("horizontal")
     container.style.padding="1rem"
     container.style.marginInline="1rem"
     
     const detrator = document.createElement("img")
-    detrator.src=links[chave_loja].nps.detratores
+    detrator.src=links.nps.detratores
     container.appendChild(detrator)
 
     const neutro = document.createElement("img")
-    neutro.src=links[chave_loja].nps.neutros
+    neutro.src=links.nps.neutros
     container.appendChild(neutro)
 
     const promotor = document.createElement("img")
-    promotor.src=links[chave_loja].nps.promotores
+    promotor.src=links.nps.promotores
     container.appendChild(promotor)
 
     document.getElementById("container").appendChild(container)
 
 }
-function historicoPublico(chave_loja){
+function historicoPublico(links){
     const historico = document.createElement("img")
-    historico.src=links[chave_loja].nps.historico_tipo_cliente
+    historico.src=links.nps.historico_tipo_cliente
     historico.classList.add("horizontal")
     historico.style.width="94%"
     document.getElementById("container").appendChild(historico)
