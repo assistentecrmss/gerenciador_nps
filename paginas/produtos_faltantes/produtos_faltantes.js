fad6033d31caad75e4a4dc227f471e5c47119bc0
Arthur Venâncio <arthurmvenancio@gmail.com>
2025-06-26 16:04:45 -0300

    refactor: ajustando produtos faltantes para rota backend

--- paginas/produtos_faltantes/produtos_faltantes.js
+++ paginas/produtos_faltantes/produtos_faltantes.js
@@ -1,22 +1,23 @@
-import links from "../../db/links.json" with {type: 'json'}
+import { pegarLink } from "../../lib/pegarLinks.js"
 import lojas from "../../db/lojas.json" with {type: 'json'}
 
-function preencherProdutosFaltantes(){
+async function preencherProdutosFaltantes(){
     const chave_loja = sessionStorage.getItem("loja_selecionada")
+    const links = await pegarLink(chave_loja)
 
     document.getElementById("titulo").textContent = lojas[chave_loja]
 
-    criarGrafico(chave_loja)
-    criarTabela(chave_loja)
+    criarGrafico(links)
+    criarTabela(links)
 }
 
-function criarGrafico(chave_loja){
+function criarGrafico(links){
     const grafico=document.getElementById("imagem")
-    grafico.src=links[chave_loja].produtos.grafico
+    grafico.src=links.produtos.grafico
 }
-function criarTabela(chave_loja){
+function criarTabela(links){
     const iframe=document.getElementById("tabela")
-    iframe.src=links[chave_loja].produtos.tabela
+    iframe.src=links.produtos.tabela
 }
 
 document.addEventListener("DOMContentLoaded", () => {
