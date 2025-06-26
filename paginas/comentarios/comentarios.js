93ab313886dc858da01ccd791f9f9e7b897adf0a
Arthur Venâncio <arthurmvenancio@gmail.com>
2025-06-26 16:07:23 -0300

    refactor: ajustando comentarios para rota backend

--- paginas/comentarios/comentarios.js
+++ paginas/comentarios/comentarios.js
@@ -1,21 +1,22 @@
-import links from "../../db/links.json" with {type: 'json'}
+import { pegarLink } from "../../lib/pegarLinks.js"
 import lojas from "../../db/lojas.json" with {type: 'json'}
 
-function preencherComentarios(){
+async function preencherComentarios() {
     const chave_loja = sessionStorage.getItem("loja_selecionada")
-
+    const links = await pegarLink(chave_loja)
+    
     document.getElementById("titulo").textContent = lojas[chave_loja]
 
-    preencherIframe(chave_loja,"30_dias")
-    preencherIframe(chave_loja,"detratores")
-    preencherIframe(chave_loja,"neutros")
-    preencherIframe(chave_loja,"promotores")
+    preencherIframe(links, "30_dias")
+    preencherIframe(links, "detratores")
+    preencherIframe(links, "neutros")
+    preencherIframe(links, "promotores")
 }
 
-function preencherIframe(chave_loja,id){
-    const iframe=document.getElementById(id)
-    iframe.src=links[chave_loja].comentarios[id]
-    iframe.style.marginInline="1rem"
+function preencherIframe(links, id) {
+    const iframe = document.getElementById(id)
+    iframe.src = links.comentarios[id]
+    iframe.style.marginInline = "1rem"
 }
 
 document.addEventListener("DOMContentLoaded", () => {
