1cdaeedd20f7a49d32b293e6850ec16a673baa61
Arthur Venâncio <arthurmvenancio@gmail.com>
2025-06-26 12:00:13 -0300

    fix: ajeitando login do usuário

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 async function verificarUsuario(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (!document.getElementById("usuario").value || !document.getElementById("senha").value) {
-        return document.getElementById("erro").innerHTML = "Preencha todos os campos"
-    }
+  if (!document.getElementById("usuario").value || !document.getElementById("senha").value) {
+    return document.getElementById("erro").innerHTML = "Preencha todos os campos"
+  }
 
-    const login = String(document.getElementById("usuario").value)
-    const password = String(document.getElementById("senha").value)
-    await fazerLogin(login, password)
+  const login = String(document.getElementById("usuario").value)
+  const password = String(document.getElementById("senha").value)
+  await fazerLogin(login, password)
 }
 
 async function fazerLogin(login, password) {
+  try {
     const response = await fetch("/api/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ login, password })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ login, password })
     });
+
+    const contentType = response.headers.get("content-type") || "";
+    if (!contentType.includes("application/json")) {
+      throw new Error("Resposta do servidor não é JSON.");
+    }
+
     const data = await response.json();
     if (response.ok) {
-        localStorage.setItem("token", data.token);
-        window.location.href = "/paginas/lojas/lojas.html"
+      localStorage.setItem("token", data.token);
+      window.location.href = "/paginas/lojas/lojas.html"
     } else {
-        document.getElementById("erro").innerHTML = data.error
+      document.getElementById("erro").innerHTML = data.error
     }
+
+  }
+  catch (error) {
+    console.error("Erro durante o login:", error);
+    document.getElementById("erro").innerText = "Erro no servidor. Tente novamente mais tarde.";
+  }
 }
 
 async function checarTokenELogar() {
@@ -50,6 +63,6 @@ async function checarTokenELogar() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    checarTokenELogar()
-    document.getElementById("login").addEventListener("submit", verificarUsuario)
+  checarTokenELogar()
+  document.getElementById("login").addEventListener("submit", verificarUsuario)
 })
